4c4ff533cc82c002014d3b48a28d949dc44e7437
Harsh Sharma <120568101+HarshSharmaIN@users.noreply.github.com>
2025-07-22 21:42:30 +0530

    Update FloatingNav.jsx

--- components/ui/FloatingNav.jsx
+++ components/ui/FloatingNav.jsx
@@ -66,7 +66,6 @@ export const FloatingNav = ({
               "relative dark:text-neutral-50 items-center  flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
             )}
           >
-              <span className="hidden lg:inline">{navItem.icon}</span>
               <span className=" text-sm !cursor-pointer">{navItem.name}</span>
           </Link>
         ))}
